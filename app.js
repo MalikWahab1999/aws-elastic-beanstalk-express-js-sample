cf0015dac06d8d155a23795facb31301079496d3
MalikWahab1999 <ahbdulwahabjrs@gmail.com>
2024-10-31 07:53:09 +0500

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Manual_Review.!'));
+app.get('/', (req, res) => res.send('i am student 21950555'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
